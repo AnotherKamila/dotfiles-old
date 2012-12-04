c48ac59f670a75816a19cb69baca5c0e408c77c6
Kamila Souckova <kamila@vesmir.sk>
2012-12-04 14:49:45 +0100

    remove .xsession

--- .config/bin/after_session_startup.sh
+++ .config/bin/after_session_startup.sh
@@ -2,8 +2,23 @@
 
 # ~/.config/bin/after_session_startup.sh
 
+# ctrl+alt+backspace should kill X
+setxkbmap -option terminate:ctrl_alt_bksp
+
+# set up input devices and screen(s)
 ~/.config/bin/peripherals.sh &
+
+# auto-locking and stuff
 ~/.config/bin/lockscreen.sh &
+
+# for urxvt(c) to load faster
+urxvtd &
+
+# kuake-like urxvt window
+#~/bin/urxvtq &
+
+# vyhadzovatko
+#~/bin/vyhadzovac.sh &
 
 # a neater notification daemon
 twmnd &
