d61c22d07b959985414d39ca1d3d2bc2a4daeb5e
Kamila Souckova <kamila@vesmir.sk>
2013-02-24 14:21:14 +0100

    add lockscreen image

--- .config/bin/lockscreen.sh
+++ .config/bin/lockscreen.sh
@@ -3,7 +3,7 @@
 
 TIME=3  # min
 # LOCKER="i3lock -u -c 000000 -i ~/lockscreen.png"
-LOCKER="bash -c 'xdotool mousemove_relative 50 -50 && i3lock -u -c 000000 -i ~/creations/lockscreen2.png'"
+LOCKER="bash -c 'xdotool mousemove_relative 50 -50 && i3lock -u -c 000000 -i ~/.config/gfx/lockscreen.png'"
 
 xautolock -time $TIME -locker "$LOCKER" -detectsleep -corners 00+- -cornerdelay 1 &
 
