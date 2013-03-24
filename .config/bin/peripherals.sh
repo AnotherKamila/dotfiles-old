fc994b42a439ecaf0d23adf99247fa14aed39ea3
Kamila Souckova <kamila@vesmir.sk>
2013-03-24 13:21:20 +0100

    natural scrolling gtk3 fix + tidying up

--- .config/bin/peripherals.sh
+++ .config/bin/peripherals.sh
@@ -7,10 +7,11 @@ setxkbmap -option compose:menu
 [[ -f $HOME/.Xmodmap ]] && xmodmap $HOME/.Xmodmap
 
 # touchpad
+# the negative *ScrollDelta values are the "natural scrolling" hack that confuses people so much
 synclient VertEdgeScroll=0 HorizEdgeScroll=0 \
 		  CircularScrolling=0 \
 		  VertTwoFingerScroll=1 HorizTwoFingerScroll=1 \
-		  VertScrollDelta=130 HorizScrollDelta=130 \
+		  VertScrollDelta=-130 HorizScrollDelta=-130 \
 		  CoastingSpeed=16 CoastingFriction=30 \
 		  EdgeMotionUseAlways=1 \
 		  ClickPad=1 \
@@ -19,24 +20,19 @@ synclient VertEdgeScroll=0 HorizEdgeScroll=0 \
 		  RightButtonAreaLeft=5000 \
 		  PalmDetect=1 \
 
-# "natural scrolling"
-xinput set-button-map 'SynPS/2 Synaptics TouchPad' 1 2 3 5 4 7 6
-
-# touchscreen-helper &
-# twofing --click=second &
-
-# trackpad
+# trackpad (both internal and on USB keyboard)
 TRACKPAD_ID=`xinput list | grep 'TrackPoint' | grep pointer | cut -f2 | cut -f2 -d'='`
 if [[ -n $TRACKPAD_ID ]]; then
-	xinput set-int-prop $TRACKPAD_ID "Evdev Wheel Emulation Axes" 8 6 7 4 5
-	xinput set-int-prop $TRACKPAD_ID "Evdev Wheel Emulation Button" 8 2
-	xinput set-int-prop $TRACKPAD_ID "Evdev Wheel Emulation" 8 1
+	xinput set-int-prop $TRACKPAD_ID 'Evdev Wheel Emulation Axes' 8 6 7 4 5
+	xinput set-int-prop $TRACKPAD_ID 'Evdev Wheel Emulation Button' 8 2
+	xinput set-int-prop $TRACKPAD_ID 'Evdev Wheel Emulation' 8 1
 fi
 
 # multi-monitor
-xrandr --output LVDS1 --auto
-if [[ -n `xrandr | grep 'VGA1 connected'` ]]; then
-	xrandr --output VGA1 --right-of LVDS1 --auto
+EXTM=`xrandr | grep '\<connected\>' | grep -v LVDS1`
+if [[ -n $EXTM ]]; then
+	xrandr --output $EXTM --above LVDS1 --auto
 else
-	xrandr --output VGA1 --off
+	for m in VGA1 HDMI1 DP1; do xrandr --output $m --off; done
 fi
+xrandr --output LVDS1 --auto
